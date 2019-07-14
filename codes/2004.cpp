5a8273ddaef033b23ce76ce027d230a2632c93dd
sihyungyou <sihyungyou@gmail.com>
2019-07-14 21:19:05 +0900

    solved 2004

--- codes/2004.cpp
+++ codes/2004.cpp
@@ -10,47 +10,33 @@ nCm의 끝자리 0의 개수를 출력하는 프로그램을 작성하시오.
 */
 
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
-// int comb(int n, int k) {
-// 	if( n == k || k == 0) return 1;
-// 	else return comb(n-1, k-1) + comb(n-1, k);
-// }
-
-long comb_dp(int n, int k) {
-	long arr[n+1][k+1];
-	int i, j = 0;
-
-	for(i = 0; i < n; i++)
-		for(j = 0; j < k; j++)
-			arr[i][j] = 0;
-
-	for(i = 0; i <= n; i++) {
-		for(j = 0; j <= k; j++) {
-			if ( i == j || j == 0) arr[i][j] = 1;
-			else arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
-		}
-	}
-
-	return arr[n][k];
+pair<long long, long long> zeros (long long n) {
+    long long two = 0, five = 0, i = 0;
+    for (i = 2; i <= n; i *= 2) two += n / i;
+    for (i = 5; i <= n; i *= 5) five += n / i;
+    return (pair <long long, long long> (two, five));
 }
 
 int main () {
-    
-    int n, r, ans, cnt, remainder = 0; 
-    cnt = 0;
+    int n, r;
+    pair<long long, long long> ansn, ansr, ansnr;
+    long long t, f;
+
     
     scanf("%d %d", &n, &r);
-    ans = comb_dp(n, r);
 
-    while(!remainder) {
-        cnt++;
-        // printf("remainder : %d\n", remainder);
-        remainder = ans%10;
-        ans /= 10;
-    }
-    printf("%d\n", cnt-1);
+    ansn = zeros(n);
+    ansr = zeros(r);
+    ansnr = zeros(n-r);
+
+    t = ansn.first - ansr.first - ansnr.first;
+    f = ansn.second - ansr.second - ansnr.second;
+
+    printf("%lld\n", min(t,f));
 
     return 0;
 }
