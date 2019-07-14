4a82a75923e7b7c744b99df50dad1f4bb6f6abaf
sihyungyou <sihyungyou@gmail.com>
2019-07-14 14:45:26 +0900

    solved 4948

--- codes/4948.cpp
+++ codes/4948.cpp
@@ -18,19 +18,38 @@ n이 주어졌을 때, n보다 크고, 2n보다 작거나 같은 소수의 개�
 */
 
 #include <cstdio>
+#include <cmath>
 
 using namespace std;
 
 int main () {
 
-    int n, n2;
-    
-    n = 1;
+    int n, n2, i, j, cnt ;
+    bool arr[246913];
+    // n = 1;
 
-    while(n != 0) {
+    while(1) {
         scanf("%d", &n);
+        if (n == 0) break;
+        cnt = 0;
         n2 = n * 2;
-        
+
+        for (i = 2; i <= n2; i++) arr[i] = true;
+
+        for (i = 2; i <= sqrt(n2); i++) {
+            if (arr[i]) {
+                for (j = i+i; j <= n2; j += i) {
+                    if (arr[j]) {
+                        arr[j] = false;
+                    }
+                }
+            }
+        }
+
+        for (i = n + 1; i <= n2; i++) {
+            if (arr[i]) cnt++;
+        }
+        printf("%d\n", cnt);
     }
 
     return 0;
