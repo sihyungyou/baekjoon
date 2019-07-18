ef205839a77b21d2e36bad5a5caaff2b0a845f5b
sihyungyou <sihyungyou@gmail.com>
2019-07-19 01:46:25 +0900

    working on 11653

--- codes/11653.cpp
+++ codes/11653.cpp
@@ -10,17 +10,38 @@ N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한
 */
 
 #include <cstdio>
+#include <cmath>
 
 using namespace std;
 
-
 int main() {
     
-    int N, i;
+    int N, i, j;
+    bool arr[10000001];
 
     scanf("%d\n", &N);
 
+    for (i = 2; i <= N; i++) arr[i] = true;
 
+    for (i = 2; i <= sqrt(N); i++) {
+        if (arr[i]) {
+            for (j = i + i; j <= N; j += i) {
+                if (arr[j]) {
+                    arr[j] = false;
+                }
+            }
+        }
+    }
+
+    if (arr[N]) printf("%d\n", N);
+    else {
+        while (!arr[N]) {
+            
+        }
+        
+    }
+    
+    
 
     return 0;
 }
