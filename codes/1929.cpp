86cc976a1200a528532d57ff79ec3bc97710e431
sihyungyou <sihyungyou@gmail.com>
2019-08-30 13:53:42 +0900

    1929

---
+++ codes/1929.cpp
@@ -0,0 +1,24 @@
+/*
+문제
+M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000)
+
+출력
+한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+*/
+
+#include <cstdio>
+#include <math>
+
+using namespace std;
+
+int main() {
+
+    int M, N, i;
+
+    
+
+    return 0;
+}
