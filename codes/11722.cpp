5fc9bd53af75311efddde479c585a5a16bfec7db
sihyungyou <sihyungyou@gmail.com>
2019-09-18 14:01:04 +0900

    solved 11722

--- codes/11722.cpp
+++ codes/11722.cpp
@@ -17,40 +17,28 @@
 
 using namespace std;
 
-int get_max(int a, int b) {
-    return a > b ? a : b;
-}
-
+int get_max(int a, int b) { return a > b ? a : b; }
 int main() {
 
-    int N, i, max = 0, ans= 0;
-    int arr[1001] = {0, };
-    int memo[1001] = {0, };
+    int N, i, j, ans = 0;
 
     scanf("%d", &N);
 
+    int arr[N];
+    int dp[N];
+
     for (i = 0; i < N; i++) scanf("%d", &arr[i]);
     
-    memo[0] = 1;
-    max = arr[0];
-    
-    for (i = 1; i < N; i++) {
-        if (arr[i] < arr[i-1]) {
-            memo[i] = memo[i-1] + 1;
-        }
-        else if (arr[i] > max) {
-            memo[i] = 1;
-            max = arr[i];
-        }
-        else {
-            memo[i] = memo[i-1];
+    for (i = 0; i < N; i++) {
+        dp[i] = 1;
+        for (j = 0; j < i; j++) {
+            if ( arr[i] < arr[j] && dp[i] < dp[j] + 1 ) dp[i] = dp[j] + 1;
         }
     }
 
-    for (i = 0; i < N; i++) ans = get_max(ans, memo[i]);
+    for (i = 0; i < N; i++) ans = get_max(ans, dp[i]);
     
     printf("%d\n", ans);
-
 
     return 0;
 }
