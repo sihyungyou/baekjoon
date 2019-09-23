810c9761e224405ffc7084a927c585dd14e43b53
sihyungyou <sihyungyou@gmail.com>
2019-09-23 16:38:21 +0900

    solving 17404

--- codes/17404.cpp
+++ codes/17404.cpp
@@ -33,24 +33,7 @@ int main() {
     dp[0][1] = arr[0][1];
     dp[0][2] = arr[0][2];
 
-    for (i = 1; i < N; i++) {
-        if (i == N-1) {
-            dp[i][0] = min(min(dp[i-1][1], dp[i-1][2]), min(dp[0][1], dp[0][2])) + arr[i][0];
-            dp[i][1] = min(min(dp[i-1][0], dp[i-1][2]), min(dp[0][0], dp[0][2])) + arr[i][1];
-            dp[i][2] = min(min(dp[i-1][1], dp[i-1][0]), min(dp[0][1], dp[0][0])) + arr[i][2];
-        } else {
-            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
-            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
-            dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + arr[i][2];   
-        }
-    }
 
-    int ans = __INT_MAX__;
-    for (i = 0; i < 3; i++) {
-        if (dp[N-1][i] < ans) ans = dp[N-1][i];
-    }
 
-    printf("%d\n", ans);
-    
     return 0;
 }
