66e3735b469251a371a803663f3341403c3d8e15
sihyungyou <sihyungyou@gmail.com>
2019-09-23 20:37:25 +0900

    solving 1912

--- codes/1912.cpp
+++ codes/1912.cpp
@@ -13,12 +13,8 @@ int main() {
     for (i = 0; i < n; i++) scanf("%d", &arr[i]);
     dp[0] = arr[0];
 
-    for (i = 1; i < n; i++) {
-        if (arr[i] + dp[i-1] < dp[i-1]) dp[i] = 0;
-        else dp[i] = dp[i-1] + arr[i];
-    }
+    for (i = 1; i < n; i++) dp[i] = max(dp[i-1] + arr[i], arr[i]);
 
-    for (i = 0; i < n; i++) ans = max(ans, dp[i]);
-    printf("%d\n", ans);
+    printf("%d\n", dp[n-1]);
     return 0;
 }
