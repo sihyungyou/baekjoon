32da3926cb78ca1bc43e02b17b709d9792863da7
sihyungyou <sihyungyou@gmail.com>
2019-10-28 00:23:47 +0900

    pa7

---
+++ codes/pa7.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+
+    int i, j, n, ans = 0;
+
+    scanf("%d", &n);
+    int arr[501][501] = { __INT_MAX__ };
+    int dp[501][501] = { __INT_MAX__ };
+
+    for (i = 1; i <= n; i++) for (j = 1; j <= n; j++) scanf("%d", &arr[i][j]);
+
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            
+        }
+    }
+
+
+    return 0;
+}
