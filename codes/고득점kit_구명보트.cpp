2db9c41d04f92e1363a9d3fa527e603a77fb818f
sihyungyou <sihyungyou@gmail.com>
2019-10-31 23:49:20 +0900

    구명보트

---
+++ codes/고득점kit_구명보트.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit) {
+    int answer = 1;
+    int i, w = 0;
+    sort(people.begin(), people.end());
+    
+    for (i = 0; i < people.size(); i++) {
+        if (w + people[i] <= limit) {
+            w += people[i];
+        }
+        else {
+            answer++;
+            w = people[i];
+        }
+    }
+    
+    return answer;
+}
