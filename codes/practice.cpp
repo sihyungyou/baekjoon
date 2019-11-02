3cb6e4d77a0bb0b29e57b6510b49629825669729
sihyungyou <sihyungyou@gmail.com>
2019-11-02 23:06:24 +0900

    연속합

--- codes/practice.cpp
+++ codes/practice.cpp
@@ -9,20 +9,19 @@ int main() {
     long long ans = 0;
     
     scanf("%d", &n);
-    int arr[n];
+    long long arr[n];
     long long dp[n];
 
-    for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+    for (i = 0; i < n; i++) scanf("%lld", &arr[i]);
     dp[0] = arr[0];
+    ans = dp[0];
 
     for (i = 1; i < n; i++) {
-        if (arr[i] + dp[i-1] > arr[i]) dp[i] = arr[i] + dp[i-1];
-        else dp[i] = arr[i];
-        printf("%lld ", dp[i]);
+        dp[i] = get_max(arr[i], dp[i-1] + arr[i]);
         ans = get_max(ans, dp[i]);
     }
 
-    printf(" >> %lld\n", ans);
+    printf("%lld\n", ans);
 
     return 0;
 }
