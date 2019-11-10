2733abe1800a8d2316f33d934b973b91b287a451
sihyungyou <sihyungyou@gmail.com>
2019-11-10 13:36:59 +0900

    2019 acmicpc 본선 Problem A

---
+++ codes/2019-ACM-ICPC-KoreaRegional-A.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <climits>
+using namespace std;
+
+int get_min (int a, int b) { return a < b ? a : b; }
+int main() {
+
+    int n, i, j, k, t1, t2, t3;
+    scanf("%d", &n);
+    int A[n+1];
+    
+    A[0] = 1;
+    A[1] = 1;
+
+    for (i = 2; i <= n; i++) {
+        t1 = 1;
+        while(1) {
+            for (k = 1; 2*k <= i; k++) { 
+                t2 = A[i-k];
+                t3 = A[i-2*k];
+                if(t1 - t2 == t2 - t3) break;
+            }
+            if (2*k > i) break;
+            t1++;
+        }
+        A[i] = t1;
+    }
+
+    printf("%d\n", A[n]);
+
+    return 0;
+}
