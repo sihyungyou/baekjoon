7b2087f6139c40a91399c185131d79f182b71cef
sihyungyou <sihyungyou@gmail.com>
2019-11-11 13:23:16 +0900

    solved 2565, solving 9251

--- codes/9251.cpp
+++ codes/9251.cpp
@@ -12,41 +12,11 @@ LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열
 */
 
 #include <cstdio>
-#include <cstring>
 using namespace std;
 
-int get_max (int a, int b) { return a > b ? a : b; }
-int get_min (int a, int b) { return a < b ? a : b; }
-
 int main() {
-    char arr1[1001];
-    char arr2[1001];
-    // int dp1[1001];
-    // int dp2[1001];
 
-    int i, j, len, ans = 0;
-    len = __INT_MAX__;
 
-    scanf("%s", arr1);
-    scanf("%s", arr2);
-
-    len = get_min(strlen(arr1), strlen(arr2));
-    dp1[len];
-    dp2[len];
-
-    for (i = 0; i < len; i++) {
-        dp1[i] = 1;
-        for (j = 0; j <= i; j++) {
-            dp2[i] = 0;
-            if (arr1[i] == arr2[j] && dp1[j] + 1 > dp1[j] && dp2[j] < dp2[i]) {
-                dp1[i] = dp1[j] + 1;
-                dp2[i] = j;
-            }
-        }
-    }
-
-    for (i = 0; i < len; i++) ans = get_max(ans, dp1[i]);
-    printf("%d\n", ans);
 
     return 0;
 }
