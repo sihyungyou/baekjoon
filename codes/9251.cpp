cf4d058fddca4f7fb8b7b9f506cc659005e95081
sihyungyou <sihyungyou@gmail.com>
2019-11-12 00:51:33 +0900

    solved 9251

--- codes/9251.cpp
+++ codes/9251.cpp
@@ -12,10 +12,32 @@ LCS(Longest Common Subsequence, 최장 공통 부분 수열)문제는 두 수열
 */
 
 #include <cstdio>
+#include <string.h>
 using namespace std;
 
+int get_max(int a, int b) { return a > b ? a : b; }
 int main() {
+    
+    char s1[1001];
+    char s2[1001];
 
+    scanf("%s %s", s1, s2);
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    int dp[len1+1][len2+1];
+    int i, j, ans = 0;
+
+    for (i = 0; i <= len1; i++) for (j = 0; j <= len2; j++) dp[i][j] = 0;
+
+    for (i = 1; i <= len1; i++) {
+        for (j = 1; j <= len2; j++) {
+            if (s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+            else dp[i][j] = get_max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    printf("%d\n", dp[len1][len2]);
 
 
     return 0;
