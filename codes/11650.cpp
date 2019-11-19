fa6eec709b48f16ee19798cc1caded507ac552dd
sihyungyou <sihyungyou@gmail.com>
2019-11-20 03:10:06 +0900

    solving 11650

---
+++ codes/11650.cpp
@@ -0,0 +1,48 @@
+/*
+문제
+2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
+
+출력
+첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
+*/
+
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+bool comp(pair<int, int> a, pair <int, int>b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+    else {
+        return a.first < b.first;
+    }
+}
+
+int main() {
+    int N, i, x, y;
+    vector<pair<int, int> >v;
+
+    scanf("%d", &N);
+
+    int arr[N][2];
+    for (i = 0; i < N; i++) {
+        scanf("%d %d", &x, &y);
+        v.push_back(make_pair(x, y));
+    }
+
+    printf("===\n");
+    sort(v.begin(), v.end(), comp);
+    
+    for (i = 0; i < N; i++) {
+        printf("%d %d\n", v[i].first, v[i].second);   
+    }
+    
+
+    return 0;
+}
