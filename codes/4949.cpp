2235583482e391d8daa70a0cf7af2cfcf9741be4
sihyungyou <sihyungyou@gmail.com>
2019-11-20 21:52:28 +0900

    solved 4949

--- codes/4949.cpp
+++ codes/4949.cpp
@@ -5,36 +5,34 @@
 using namespace std;
 
 int main() {
-    int flag;
 
     while(1) {
         char input[101];
-        stack <char> small, big;
-        cin.getline(input, 100);
+        cin.getline(input, 101);
+
         if (!strcmp(input, ".")) break;
-        flag = 0;
+
+        stack <char> s;
+        int flag = 0;
 
         for (int i = 0; i < strlen(input); i++) {
-            if (input[i] == '(') small.push('(');
-            else if (input[i] == '[') big.push('[');
+            if (input[i] == '(' || input[i] == '[') s.push(input[i]);
 
             else if (input[i] == ')') {
-                if (!big.empty()) { cout << "no\n"; flag = 1; break; }
-
-                if (!small.empty()) small.pop();
-                else { cout << "NO\n"; flag = 1; break; }
+                if (!s.empty() && s.top() == '(') s.pop();
+                else { cout << "no\n"; flag = 1; break; }
             }
 
             else if (input[i] == ']') {
-                if (!small.empty()) { cout << "no\n"; flag = 1; break; }
-
-                if (!big.empty()) big.pop();
-                else { cout << "NO\n"; flag = 1; break; }
+                if (!s.empty() && s.top() == '[') s.pop();
+                else { cout << "no\n"; flag = 1; break; }
             }
         }
+
         if (flag) continue;
-        if (!small.empty() || !big.empty()) cout << "NO\n";
-        else cout << "YES\n";
+
+        if (!s.empty()) cout << "no\n";
+        else cout << "yes\n";
     }
 
 
