8a1876dc3c629510646b07ea41a6a46047aa0a2f
sihyungyou <sihyungyou@gmail.com>
2019-11-25 16:24:41 +0900

    solved 17298

--- codes/17298.cpp
+++ codes/17298.cpp
@@ -10,3 +10,37 @@
 출력
 총 N개의 수 NGE(1), NGE(2), ..., NGE(N)을 공백으로 구분해 출력한다.
 */
+
+#include <cstdio>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main() {
+
+    int N, i, temp;
+    vector <int> v;
+    stack <int> s;
+
+    scanf("%d", &N);
+    for (i = 0; i < N; i++) {
+        scanf("%d", &temp);
+        v.push_back(temp);
+    }
+
+    vector <int> ans(v.size(), -1);
+
+    for (i = 0; i < v.size(); i++) {
+        while(!s.empty() && v[s.top()] < v[i]) {
+            ans[s.top()] = v[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    for (i = 0; i < ans.size(); i++) printf("%d ", ans[i]);
+    printf("\n");
+
+    return 0;
+}
