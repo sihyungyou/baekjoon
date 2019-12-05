cac1323379d633cd36fa413faaee6f9b84d6ff64
sihyungyou <sihyungyou@gmail.com>
2019-12-06 04:01:42 +0900

    1946번 신입 사원

--- 백준/1946.cpp
+++ 백준/1946.cpp
@@ -24,6 +24,8 @@ bool comp(pair<int, int> &a, pair<int, int> &b) {
     return a.first < b.first;
 }
 
+int get_min(int a, int b) { return a < b ? a : b;}
+
 int main() {
 
     int t, a, b, i, j, k, n;
@@ -34,19 +36,22 @@ int main() {
     for (i = 0; i < t; i++) {
         scanf("%d", &n);
         v.clear();
+
         for (j = 0; j < n; j++) {
-            // vector<pair<int, int> > v;
             scanf("%d %d", &a, &b);
             v.push_back(make_pair(a, b));
         }
+
         sort(v.begin(), v.end(), comp);
-        printf("===\n");
-        for (int k = 0; k < v.size(); k++) printf("%d %d\n", v[k].first, v[k].second);
-        int cnt = v.size();
-        for (k = 1; k < v.size(); k++) {
-            if (v[k].second > v[0].second) cnt--;
+        int cnt = n;
+        int bound = v[0].second;
+
+        for (k = 1; k < n; k++) {
+            if (v[k].second > bound) cnt--;
+            bound = get_min(bound, v[k].second);
         }
-        printf(">> %d\n", cnt);
+
+        printf("%d\n", cnt);
     }
 
     return 0;
