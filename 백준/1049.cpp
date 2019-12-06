24f2fff50e1be6f8625799a776c670da34af9cea
sihyungyou <sihyungyou@gmail.com>
2019-12-06 12:52:27 +0900

    1049번 기타줄

--- 백준/1049.cpp
+++ 백준/1049.cpp
@@ -18,19 +18,27 @@ using namespace std;
 int get_min(int a, int b) { return a < b ? a : b; }
 int main() {
 
-    int n, m, i, package, single, pquant, price = __INT_MAX__;
-
+    int n, m, i, package = INT_MAX, single = INT_MAX, pquant, temp, temp1, temp2, mix, allp, alls, price = __INT_MAX__;
+    bool flag;
     scanf("%d %d", &n, &m);
 
     for (i = 0; i < m; i++) {
-        scanf("%d %d", &package, &single);
-        if (n % 6 == 0) pquant = n/6;
-        else pquant = n/6 + 1;
-
-        int temp = get_min(pquant * package, n * single);
-        price = get_min(price, temp);
+        flag = false;
+        scanf("%d %d", &temp1, &temp2);
+        package = get_min(package, temp1);
+        single = get_min(single, temp2);
     }
 
+    if (n % 6 == 0) flag = true;
+    pquant = n/6;
+    mix = (pquant * package) + (n - pquant * 6) * single;
+    if (flag) allp = pquant * package;
+    else allp = (pquant + 1) * package;
+    alls = single * n;
+    
+    temp = get_min(mix, allp);
+    temp = get_min(temp, alls);
+    price = get_min(price, temp);
 
     printf("%d\n", price);
 
