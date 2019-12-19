c78ef7f6d3c8654ca01a811c2c3d18a27d02432e
sihyungyou <sihyungyou@gmail.com>
2019-12-19 19:48:57 +0900

    글로벌 라운드 6 B번 Dice Tower

--- 대회/코드포스/Global_6/B.cpp
+++ 대회/코드포스/Global_6/B.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main() {
 
-    int n, i, j, num;
+    int n, i, j;
+    long long num;
     scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
-        scanf("%d", &num);
+        scanf("%lld", &num);
+
         for (j = 1; j < 7; j++) {
-            if ( (((num - j) % 14) == 0)) {
+            if ( (num > 14) && ((num - j) % 14) == 0 ) {
                 printf("YES\n");
                 break;
             }
