25754b90e1a4da570c7fb46db409cf4b7ef5855f
sihyungyou <sihyungyou@gmail.com>
2019-12-20 01:51:55 +0900

    1436번 영화감독 숀

---
+++ 백준/1436.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+    int i = 666, n, cnt = 0;
+    bool flag;
+    scanf("%d", &n);
+
+    while(cnt != n) {
+        int temp = i;
+        flag = false;
+
+        while(temp != 0) {
+            if (temp % 1000 == 666) {
+                flag = true;
+                break;
+            }
+            temp /= 10;
+        }
+
+        if (flag) cnt++;
+        i++;
+    }
+
+    printf("%d\n", i-1);
+
+
+    return 0;
+}
