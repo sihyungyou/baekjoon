bab191645310d29f6c9be3ae6fae11b129e5c8ba
sihyungyou <sihyungyou@gmail.com>
2019-12-25 00:48:30 +0900

    계단오르기 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -4,35 +4,24 @@ using namespace std;
 int get_max(int a, int b) { return a > b ? a : b; }
 
 int main() {
-
-    int i, j, n, t;
-    long long ans = 0;
-    long long dp[500][500];
+    
+    int n, i;
+    int arr[300];
+    long long dp[300];
 
     scanf("%d", &n);
 
-    for (t = 0; t < n; t++) {
-        for (i = 0; i <= t; i++) {
-            scanf("%lld", &dp[t][i]);
-        }
+    for (i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    dp[0] = arr[0];
+    dp[1] = get_max(arr[1], arr[0] + arr[1]);
+    dp[2] = get_max(arr[0] + arr[2], arr[1] + arr[2]);
+
+    for (i = 3; i < n; i++) {
+        dp[i] = get_max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
     }
 
-    dp[1][0] += dp[0][0];
-    dp[1][1] += dp[0][0];
-
-    for (i = 2; i < n; i++) {
-        dp[i][0] += dp[i-1][0];
-        dp[i][i] += dp[i-1][i-1];
-        for (j = 1; j < i; j++) {
-            dp[i][j] += get_max(dp[i-1][j-1], dp[i-1][j]);
-        }
-    }
-
-    for (i = 0; i < n; i++) {
-        ans = get_max(dp[n-1][i], ans);
-    }
-
-    printf("%lld\n", ans);
-
+    printf("%lld\n", dp[n-1]);
+    
     return 0;
 }
