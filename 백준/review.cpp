913907a62f82348f5709c90baeada57df94c2954
sihyungyou <sihyungyou@gmail.com>
2019-12-24 23:06:17 +0900

    RGB거리 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -1,26 +1,26 @@
 #include <cstdio>
 
 using namespace std;
-
+int get_min(int a, int b) { return a < b ? a : b; }
 int main() {
 
-    long long dp[101] = {0, };
-    int t, i, n;
+    int i, j, n, ans;
 
-    dp[1] = 1;
-    dp[2] = 1;
-    dp[3] = 1;
-    dp[4] = 2;
-    dp[5] = 2;
+    int dp[1000][3];
 
-    for (i = 6; i < 101; i++) dp[i] = dp[i-1] + dp[i-5];
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) scanf("%d %d %d", &dp[i][0], &dp[i][1], &dp[i][2]);
 
-    scanf("%d", &t);
-
-    for (i = 0; i < t; i++) {
-        scanf("%d", &n);
-        printf("%lld\n", dp[n]);
+    for (i = 1; i < n; i++) {
+        dp[i][0] += get_min(dp[i-1][1], dp[i-1][2]);
+        dp[i][1] += get_min(dp[i-1][0], dp[i-1][2]);
+        dp[i][2] += get_min(dp[i-1][1], dp[i-1][0]);
     }
+    
+    ans = get_min(dp[n-1][0], dp[n-1][1]);
+    ans = get_min(ans, dp[n-1][2]);
+
+    printf("%d\n", ans);
 
     return 0;
 }
