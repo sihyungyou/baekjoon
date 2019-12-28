04008c25eb4a87e61c8142134be4dc712a389fc6
sihyungyou <sihyungyou@gmail.com>
2019-12-28 09:20:24 +0900

    가장 긴 바이토닉 부분 수열 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -8,17 +8,24 @@ int main() {
 
     scanf("%d", &n);
     int dp[n];
+    int dp2[n];
     int arr[n];
     for (i = 0; i < n; i++) scanf("%d", &arr[i]);
 
     for (i = 0; i < n; i++) {
         dp[i] = 1;
-        for (j = 0; j < i; j++) {
-            if (dp[j] + 1 > dp[i] && arr[j] < arr[i]) dp[i] = dp[j] + 1;
-        }
-        ans = get_max(ans, dp[i]);
+        for (j = 0; j < i; j++) if (dp[j] + 1 > dp[i] && arr[j] < arr[i]) dp[i] = dp[j] + 1;
     }
 
-    printf("%d\n", ans);
+    for (i = n-1; i >= 0; i--) {
+        dp2[i] = 1;
+        for (j = n-1; j > i; j--) if(dp2[j] + 1 > dp2[i] && arr[j] < arr[i]) dp2[i] = dp2[j] + 1;
+    }
+
+    for (i = 0; i < n; i++) {
+        ans = get_max(ans, dp[i] + dp2[i]);
+    }
+
+    printf("%d\n", ans-1);
     return 0;
 }
