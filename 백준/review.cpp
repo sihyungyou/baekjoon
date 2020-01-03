d6007ad5b64e91f194915e3879aa1c1ec5112110
sihyungyou <sihyungyou@gmail.com>
2020-01-03 20:08:48 +0900

    2252번 줄 세우기 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -1,35 +1,44 @@
 #include <cstdio>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-bool graph[101][101] = { 0, };
-bool v[101] = { false, };
-int n, m, i, src, dest, cnt = 0;
-
-void dfs(int s) {
-    int i;
-    v[s] = true;
-
-    for (i = 1; i <= n; i++) {
-        if (graph[s][i] && !v[i]) {
-            cnt++;
-            dfs(i);
-        }
-    }
-}
-
 int main() {
 
+    int n, m, i, first, second, temp;
+
+    vector<int> v[32001];
+    queue<int> q;
+
     scanf("%d %d", &n, &m);
+    int degree[n+1];
+
+    for (i = 0; i <= n; i++) degree[i] = 0;
 
     for (i = 0; i < m; i++) {
-        scanf("%d %d", &src, &dest);
-        graph[src][dest] = true;
-        graph[dest][src] = true;
+        scanf("%d %d", &first, &second);
+        degree[second]++;
+        v[first].push_back(second);
     }
 
-    dfs(1);
-    printf("%d\n", cnt);
+
+    for (i = 1; i <= n; i++) {
+        if (!degree[i]) q.push(i);
+    }
+
+    while (!q.empty()) {
+        temp = q.front();
+        q.pop();
+        printf("%d ", temp);
+
+        for (i = 0; i < v[temp].size(); i++) {
+            degree[v[temp][i]]--;
+            if (!degree[v[temp][i]]) q.push(v[temp][i]);
+        }
+    }
+
+    printf("\n");
 
     return 0;
 }
