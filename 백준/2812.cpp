08ffaaa754ab4d71d18f365e90bc5fa2cd1ba9b2
sihyungyou <sihyungyou@gmail.com>
2020-01-04 17:22:07 +0900

    2812번 크게 만들기

--- 백준/2812.cpp
+++ 백준/2812.cpp
@@ -1,37 +1,43 @@
+/*
+문제
+N자리 숫자가 주어졌을 때, 여기서 숫자 K개를 지워서 얻을 수 있는 가장 큰 수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 N과 K가 주어진다. (1 ≤ K < N ≤ 500,000)
+
+둘째 줄에 N자리 숫자가 주어진다. 이 수는 0으로 시작하지 않는다.
+
+출력
+입력으로 주어진 숫자에서 K개를 지웠을 때 얻을 수 있는 가장 큰 수를 출력한다.
+*/
+
 #include <cstdio>
 #include <cstring>
+#include <queue>
+
 using namespace std;
 
 int main() {
 
-    int n, k, i, j, cnt = 0;
+    int n, k, i;
     char str[500001];
+    deque<char> dq;
 
     scanf("%d %d", &n, &k);
     scanf("%s", str);
 
     for (i = 0; i < n; i++) {
-        if (str[i] <= str[i+1] && cnt < k) {
-            str[i] = '-';
-            cnt++;
+        while(k && !dq.empty() && dq.back() < str[i]) {
+            dq.pop_back();
+            k--;
         }
-        else if (n - i <= k - cnt) { 
-            str[i] = '-';
-            cnt++;
-        }
-        else {
-            for (j = 0; j < k - cnt; j++) {
-                if (str[i] < str[j]) {
-                    str[i] = '-';
-                    cnt++;
-                }
-            }
-        }
+        dq.push_back(str[i]);
     }
 
-    for (i = 0; i < n; i++) {
-        if (str[i] != '-') printf("%c", str[i]);
+    for (i = 0; i < dq.size() - k; i++) {
+        printf("%c", dq[i]);
     }
+
     printf("\n");
 
     return 0;
