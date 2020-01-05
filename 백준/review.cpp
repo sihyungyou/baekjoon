19262892ae02498a8d909ae80d00289b30ab2aba
sihyungyou <sihyungyou@gmail.com>
2020-01-05 23:27:37 +0900

    11286번 절댓값 힙 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -1,36 +1,29 @@
 #include <cstdio>
-#include <climits>
+#include <cmath>
+#include <queue>
+#include <utility>
 
 using namespace std;
 
-int get_min(int a, int b) { return a < b ? a : b; }
-
 int main() {
 
-    int n, k, i, j;
+    priority_queue<pair <int, int>, vector<pair<int, int> >, greater<pair<int, int> > >pq;    
+    int n, i, temp;
 
-    scanf("%d %d", &n, &k);
-
-    int dp[100001];
-    int coins[100];
-
-    dp[0] = 0;
-    for (i = 1; i <= k; i++) dp[i] = INT_MAX;
+    scanf("%d", &n);
 
     for (i = 0; i < n; i++) {
-        scanf("%d", &coins[i]);
-        dp[coins[i]] = 1;
-    }
-
-    for (i = 1; i <= k; i++) {
-        for (j = 0; j < n; j++) {
-            if (dp[i] == 1) break;
-            else if (i - coins[j] < 0 || dp[i - coins[j]] == INT_MAX) continue;
-            else dp[i] = get_min(dp[i], 1 + dp[i - coins[j]]);
+        scanf("%d", &temp);
+        if (temp == 0) {
+            if (pq.empty()) printf("0\n");
+            else {
+                printf("%d\n", pq.top().second);
+                pq.pop();
+            }
         }
+        else pq.push(make_pair(abs(temp), temp));
+        
     }
 
-    if (dp[k] == INT_MAX) printf("-1\n");
-    else printf("%d\n", dp[k]);
     return 0;
 }
