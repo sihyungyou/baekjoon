fe0ad1d4b984c292fe5cbbf255932d0e7d32a3dd
sihyungyou <sihyungyou@gmail.com>
2020-01-09 00:25:24 +0900

    2312번 수 복원하기 복습

--- 백준/review.cpp
+++ 백준/review.cpp
@@ -1,21 +1,30 @@
 #include <cstdio>
+#include <map>
 
 using namespace std;
 
 int main() {
 
-    int n, i, j;
-    scanf("%d", &n);
+    int t, T, n, i;
 
-    i = 2;
-    
-    while(n != 1) {
-        if (n % i == 0) {
-            printf("%d\n", i);
-            n /= i;
-            i = 2;
+    scanf("%d", &T);
+
+    for (t = 0; t < T; t++) {
+        scanf("%d", &n);
+        map<int, int> m;
+
+        i = 2;
+        while(n != 1) {
+            if (n % i == 0) {
+                m[i]++;
+                n /= i;
+                i = 2;
+            }
+            else i++;
         }
-        else i++;
+
+        for(map<int, int>::iterator it = m.begin(); it != m.end(); it++) printf("%d %d\n", it->first, it->second);
+
     }
 
     return 0;
